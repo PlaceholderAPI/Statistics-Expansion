9bf9437f1b8c2d68740470bcfeb8014ed12818e5
Kqliber <kqliber@gmail.com>
2025-03-28 23:13:05 +0000

    fix duplicating statistics

--- src/main/java/com/extendedclip/papi/expansion/mcstatistics/StatisticsExpansion.java
+++ src/main/java/com/extendedclip/papi/expansion/mcstatistics/StatisticsExpansion.java
@@ -264,6 +264,10 @@ public class StatisticsExpansion extends PlaceholderExpansion implements Cacheab
                 continue;
             }
 
+            if (material.name().startsWith("LEGACY_")) {
+                continue;
+            }
+
             if (statistic == Statistic.MINE_BLOCK && (material.name().equals("GRASS") || material.name().equals("SOIL"))) {
                 continue;
             }
